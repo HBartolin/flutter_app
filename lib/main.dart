b436bc11664019cb3ec39fa28d975e4f6d7e92f1
Hrvoje Bartolin <hbartolin@gmail.com>
2023-09-26 13:12:29 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My first app"),
-        centerTitle: true,
-        backgroundColor: Colors.red.shade600,
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]
       ),
-      body: Center(
-        child: Image.asset('assets/s1.jpg'),
-        /* child: Text(
-          'hello',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey.shade600,
-            fontFamily: 'IndieFlower',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text('hello, world'),
+          FloatingActionButton(
+            onPressed: () {},
+            splashColor: Colors.amber,
+            child: Text('click me'),
           ),
-        ), */
+          Container(
+              color: Colors.cyan,
+              padding: EdgeInsets.all(30.0),
+              child: Text('inside container')
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.red.shade600,
-        child: const Text('+'),
+        backgroundColor: Colors.red[600],
+        onPressed: () {  },
+        child: Text('click'),
       ),
     );
   }
