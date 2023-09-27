ad2f0261c8dbdb7c605c0ed42bbce335a483f33c
Hrvoje Bartolin <hbartolin@gmail.com>
2023-09-27 12:31:05 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MaterialApp(
   home: Home(),
 ));
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('my first app'),
+          title: const Text('my first app'),
           centerTitle: true,
           backgroundColor: Colors.red[600]
       ),
@@ -18,26 +20,26 @@ class Home extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             color: Colors.cyan,
-            child: Text("one"),
+            child: const Text("one"),
           ),
           Container(
-            padding: EdgeInsets.all(30),
+            padding: const EdgeInsets.all(30),
             color: Colors.green,
-            child: Text("two"),
+            child: const Text("two"),
           ),
           Container(
-            padding: EdgeInsets.all(40),
+            padding: const EdgeInsets.all(40),
             color: Colors.yellow,
-            child: Text("three"),
+            child: const Text("three"),
           )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red[600],
         onPressed: () {  },
-        child: Text('click'),
+        child: const Text('click'),
       ),
     );
   }
